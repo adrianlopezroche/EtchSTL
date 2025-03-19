0bd9344fc56519d0d38cbdd6df4831171d0c03fc
Adrian Lopez <adrianlopezroche@gmail.com>
2025-03-19 14:32:12 -0400

    Refactor mesh-generation code and fix issue with sides for pixel pitch = 0.

--- etchstl.py
+++ etchstl.py
@@ -99,6 +99,7 @@ def createmesh(image):
     total_width = image_width * (pixel_size + pixel_separation) + pixel_separation
     total_height = image_height * (pixel_size + pixel_separation) + pixel_separation
 
+    # front vertices
     appendline(vertices, total_height, thickness, image_width)
 
     for y in range(0, image_height):
@@ -110,6 +111,7 @@ def createmesh(image):
 
     appendline(vertices, 0, thickness, image_width)
 
+    # middle vertices
     insetindexoffset = len(vertices)
 
     appendline(vertices, total_height, thickness - depth, image_width)
@@ -123,6 +125,7 @@ def createmesh(image):
 
     appendline(vertices, 0, thickness - depth, image_width)
 
+    # back vertices
     bottomoffset = len(vertices)
 
     vertices.append((0, thickness, 0))
@@ -130,68 +133,195 @@ def createmesh(image):
     vertices.append((total_width, thickness, total_height))
     vertices.append((0, thickness, total_height))
 
-    for y in range(0, image_height):
-        line0 = (y*2 + 1) * (image_width*2 + 2)
-        line1 = (y*2 + 2) * (image_width*2 + 2)
+    vertex_count_x = image_width * 2 + 2
+    vertex_count_y = image_height * 2 + 2
 
-        insetline0 = insetindexoffset + (y*2 + 1) * (image_width*2 + 2)
-        insetline1 = insetindexoffset + (y*2 + 2) * (image_width*2 + 2)
+    # structural support faces
+    if pixel_separation > 0:
+        for y in range(0, vertex_count_y, 2):
+            line0 = y * vertex_count_x
+            line1 = (y + 1) * vertex_count_x
 
-        for x in range(0, image_width):
-            p = image.getpixel((x,y))
+            for x in range(0, vertex_count_x - 1):
+                vf0 = line0 + x
+                vf1 = line0 + x + 1
+                vb0 = line1 + x
+                vb1 = line1 + x + 1
 
-            if p == 255:
-                triangles.append((line1 + x*2 + 1, line0 + x*2 + 2, line0 + x*2 + 1))
-                triangles.append((line1 + x*2 + 1, line1 + x*2 + 2, line0 + x*2 + 2))
+                triangles.append((vf0, vb0, vf1))
+                triangles.append((vf1, vb0, vb1))
+
+        for y in range(1, vertex_count_y - 1, 2):
+            line0 = y * vertex_count_x
+            line1 = (y + 1) * vertex_count_x
+
+            for x in range(0, vertex_count_x - 1, 2):
+                vf0 = line0 + x
+                vf1 = line0 + x + 1
+                vb0 = line1 + x
+                vb1 = line1 + x + 1
+
+                triangles.append((vf0, vb0, vf1))
+                triangles.append((vf1, vb0, vb1))
+
+    # pixels
+    for py in range(0, image_height):
+        y = py * 2 + 1
+        line0 = y * vertex_count_x
+        line1 = (y + 1) * vertex_count_x
+
+        for px in range(0, image_width):
+            x = px * 2 + 1
+
+            vf0 = line0 + x
+            vf1 = line1 + x
+            vf2 = line1 + x + 1
+            vf3 = line0 + x + 1
+            vb0 = insetindexoffset + line0 + x
+            vb1 = insetindexoffset + line1 + x
+            vb2 = insetindexoffset + line1 + x + 1
+            vb3 = insetindexoffset + line0 + x + 1
+
+            if image.getpixel((px, py)) == 255:
+                triangles.append((vf0, vf1, vf3))
+                triangles.append((vf3, vf1, vf2))
+
+                if pixel_separation <= 0:
+                    if px == 0 or image.getpixel((px - 1, py)) != 255:
+                        triangles.append((vf0, vb0, vb1))
+                        triangles.append((vb1, vf1, vf0))
+
+                    if px + 1  == image_width or image.getpixel((px + 1, py)) != 255:
+                        triangles.append((vb3, vf3, vf2))
+                        triangles.append((vf2, vb2, vb3))
+
+                    if py == 0 or image.getpixel((px, py - 1)) != 255:
+                        triangles.append((vb3, vb0, vf0))
+                        triangles.append((vf0, vf3, vb3))
+
+                    if py + 1 == image_height or image.getpixel((px, py + 1)) != 255:
+                        triangles.append((vb2, vf1, vb1))
+                        triangles.append((vb2, vf2, vf1))
             else:
-                triangles.append((insetline1 + x*2 + 1, insetline0 + x*2 + 2, insetline0 + x*2 + 1))
-                triangles.append((insetline1 + x*2 + 1, insetline1 + x*2 + 2, insetline0 + x*2 + 2))
+                triangles.append((vb0, vb1, vb3))
+                triangles.append((vb3, vb1, vb2))
 
-                if pixel_separation > 0 or y == 0 or image.getpixel((x, y - 1)) != p:
-                    triangles.append((insetline0 + x*2 + 1, line0 + x*2 + 2, line0 + x*2 + 1))
-                    triangles.append((insetline0 + x*2 + 1, insetline0 + x*2 + 2, line0 + x*2 + 2))
+                if pixel_separation > 0:
+                    triangles.append((vb1, vb0, vf0))
+                    triangles.append((vf0, vf1, vb1))
 
-                if pixel_separation > 0 or y + 1 == image_height or image.getpixel((x, y + 1)) != p:
-                    triangles.append((line1 + x*2 + 1, line1 + x*2 + 2, insetline1 + x*2 + 1))
-                    triangles.append((line1 + x*2 + 2, insetline1 + x*2 + 2, insetline1 + x*2 + 1))
+                    triangles.append((vf2, vf3, vb3))
+                    triangles.append((vb3, vb2, vf2))
 
-                if pixel_separation > 0 or x == 0 or image.getpixel((x - 1, y)) != p:
-                    triangles.append((line0 + x*2 + 1, line1 + x*2 + 1, insetline0 + x*2 + 1))
-                    triangles.append((insetline1 + x * 2 + 1, insetline0 + x*2 + 1, line1 + x*2 + 1))
+                    triangles.append((vf0, vb0, vb3))
+                    triangles.append((vb3, vf3, vf0))
 
-                if pixel_separation > 0 or x + 1 == image_width or image.getpixel((x + 1, y)) != p:
-                    triangles.append((insetline0 + x*2 + 2, line1 + x*2 + 2, line0 + x*2 + 2))
-                    triangles.append((line1 + x*2 + 2, insetline0 + x*2 + 2, insetline1 + x * 2 + 2))
+                    triangles.append((vb1, vf1, vb2))
+                    triangles.append((vf1, vf2, vb2))
 
-    for y in range(0, image_height * 2 + 1):
-        for x in range(0, image_width * 2 + 1):
-            line0 = (y) * (image_width * 2 + 2)
-            line1 = (y + 1) * (image_width * 2 + 2)
+    # structural support perimeter
+    if pixel_separation > 0:
+        for y in range(0, vertex_count_y - 1):
+            line0 = y * vertex_count_x
+            line1 = (y + 1) * vertex_count_x
 
-            if y % 2 == 0 or x % 2 == 0:
-                triangles.append((line1 + x, line0 + x + 1, line0 + x))
-                triangles.append((line1 + x, line1 + x + 1, line0 + x + 1))
+            v0 = line0
+            v1 = line1
+            v2 = insetindexoffset + line0
+            v3 = insetindexoffset + line1
 
-    triangles.append((bottomoffset + 2, bottomoffset + 1, bottomoffset + 0))
-    triangles.append((bottomoffset + 3, bottomoffset + 2, bottomoffset + 0))
+            triangles.append((v0, v2, v3))
+            triangles.append((v3, v1, v0))
 
-    for y in range(0, image_height * 2 + 1):
-        line0 = (y) * (image_width*2 + 2)
-        line1 = (y + 1) * (image_width*2 + 2)
+            v0 += vertex_count_x - 1
+            v1 += vertex_count_x - 1
+            v2 += vertex_count_x - 1
+            v3 += vertex_count_x - 1
 
-        triangles.append((line1, line0, bottomoffset))
+            triangles.append((v3, v2, v0))
+            triangles.append((v0, v1, v3))
 
-        triangles.append((bottomoffset + 1, line0 + (image_width * 2) + 1, line1 + (image_width * 2) + 1))
+        for x in range(0, vertex_count_x - 1):
+            v0 = x
+            v1 = x + 1
+            v2 = insetindexoffset + x
+            v3 = insetindexoffset + x + 1
 
-    triangles.append((bottomoffset + 3, bottomoffset, 0))
-    triangles.append((bottomoffset + 1, bottomoffset + 2, image_width * 2 + 1))
+            triangles.append((v0, v1, v3))
+            triangles.append((v3, v2, v0))
 
-    for x in range(0, image_width * 2 + 1):
-        triangles.append((bottomoffset + 3, x, x + 1))
-        triangles.append(((image_height * 2 + 1) * (image_width * 2 + 2) + x + 1, (image_height * 2 + 1) * (image_width * 2 + 2) + x, bottomoffset))
+            v0 += (vertex_count_y - 1) * vertex_count_x
+            v1 += (vertex_count_y - 1) * vertex_count_x
+            v2 += (vertex_count_y - 1) * vertex_count_x
+            v3 += (vertex_count_y - 1) * vertex_count_x
 
-    triangles.append((image_width * 2 + 1, bottomoffset + 2, bottomoffset + 3))
-    triangles.append((bottomoffset, bottomoffset + 1, (image_height * 2 + 1) * (image_width * 2 + 2) + image_width * 2 + 1))
+            triangles.append((v3, v1, v0))
+            triangles.append((v0, v2, v3))
+
+    # plate sides (back section)
+    for y in range(0, vertex_count_y - 1):
+        line0 = y * vertex_count_x
+        line1 = (y + 1) * vertex_count_x
+
+        v0 = insetindexoffset + line0
+        v1 = insetindexoffset + line1
+        v2 = bottomoffset
+
+        triangles.append((v2, v1, v0))
+
+        v0 += vertex_count_x - 1
+        v1 += vertex_count_x - 1
+        v2 = bottomoffset + 2
+
+        triangles.append((v0, v1, v2))
+
+    v0 = bottomoffset
+    v1 = bottomoffset + 3
+    v2 = insetindexoffset
+
+    triangles.append((v1, v0, v2))
+
+    v0 = bottomoffset + 1
+    v1 = bottomoffset + 2
+    v2 = insetindexoffset + (vertex_count_y - 1) * vertex_count_x + vertex_count_x - 1
+
+    triangles.append((v0, v1, v2))
+
+    for x in range(0, vertex_count_x - 1):
+        last_y = (vertex_count_y - 1) * vertex_count_x
+
+        v0 = insetindexoffset + x
+        v1 = insetindexoffset + x + 1
+        v2 = bottomoffset + 2
+
+        triangles.append((v0, v1, v2))
+
+        v0 = insetindexoffset + last_y + x
+        v1 = insetindexoffset + last_y + x + 1
+        v2 = bottomoffset
+
+        triangles.append((v1, v0, v2))
+
+    v0 = bottomoffset + 2
+    v1 = bottomoffset + 3
+    v2 = insetindexoffset
+
+    triangles.append((v0, v1, v2))
+
+    v0 = bottomoffset
+    v1 = bottomoffset + 1
+    v2 = insetindexoffset + (vertex_count_y - 1) * vertex_count_x + vertex_count_x - 1
+
+    triangles.append((v0, v1, v2))
+
+    # plate back
+    v0 = bottomoffset
+    v1 = bottomoffset + 1
+    v2 = bottomoffset + 2
+    v3 = bottomoffset + 3
+
+    triangles.append((v0, v3, v2))
+    triangles.append((v2, v1, v0))
 
     return vertices, triangles
 
